e017463a3919dc29755938705903d45fd7f7dd5d
vbondoux <vbondoux@gmail.com>
2025-03-29 08:09:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ load_dotenv()
 app = FastAPI()
 
 MAKE_WEBHOOK_URL = os.getenv('MAKE_WEBHOOK_URL')
-TARGET_CUSTOM_IDS = os.getenv('TARGET_CUSTOM_IDS', "").split(",")
+TARGET_CAMPAIGN_IDS = os.getenv('TARGET_CAMPAIGN_IDS', "").split(",")
 
 @app.get("/")
 async def root():
@@ -18,9 +18,9 @@ async def root():
 async def filter_mailjet(request: Request):
     payload = await request.json()
 
-    event_custom_id = payload.get('CustomID')
+    event_campaign_id = str(payload.get('CampaignID'))
 
-    if event_custom_id in TARGET_CUSTOM_IDS:
+    if event_campaign_id in TARGET_CAMPAIGN_IDS:
         # Transmet l'événement vers Make
         async with httpx.AsyncClient() as client:
             try:
@@ -29,6 +29,12 @@ async def filter_mailjet(request: Request):
             except httpx.HTTPError as e:
                 raise HTTPException(status_code=500, detail=f"Erreur en envoyant vers Make: {str(e)}")
 
-        return {"status": "forwarded to Make", "custom_id": event_custom_id}
+        return {
+            "status": "forwarded to Make",
+            "campaign_id": event_campaign_id
+        }
     else:
-        return {"status": "ignored", "custom_id": event_custom_id}
+        return {
+            "status": "ignored",
+            "campaign_id": event_campaign_id
+        }
